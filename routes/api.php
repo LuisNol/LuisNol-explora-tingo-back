4d8d8c59c1a54e0e826bb5fc34b9afff014ff5e6
Nolberto Luis Sumaran Pimentel <nolberto.sumaran@gmail.com>
2026-07-20 19:10:18 -0500

    USER_ROLE

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,20 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Role\RoleController;
-use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Role\RoleController;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
+use App\Http\Controllers\User\UserController;
 
-
-
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+ 
 Route::group([
-     //'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    //Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-   // Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
-  //  Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
-   // Route::post('/me', [AuthController::class, 'me'])->middleware(['auth:api','permission:delete_user'])->name('me');
 });
 Route::group([
     'middleware' => "auth:api"
@@ -30,5 +24,6 @@ Route::group([
     Route::post("users/{id}",[UserController::class,"update"]);
     Route::resource("users",UserController::class);
 
-    
+
 });
+
